96a6491d8e555200ad315432561550fcb745a7b9
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-11-03 19:45:41 +0300

    fix: Подправил валидацию

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -35,13 +35,13 @@ module.exports.validateRegistration = celebrate({
   [Segments.BODY]: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 });
 
 module.exports.validateUpdatingUser = celebrate({
   [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
   }),
 });
